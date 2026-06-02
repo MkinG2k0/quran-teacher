785c4670a35e15abacc58385603388a4b12a4482
mk <kamai122000@mail.ru>
2026-06-03 02:06:16 +0300

    dd

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic'
+
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<div className="dark min-h-screen bg-[#0D1117] text-[#E8E0D0]">
