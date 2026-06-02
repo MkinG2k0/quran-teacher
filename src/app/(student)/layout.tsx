7d5c369b8e83076b6dee081ae1ab25777ca87efd
mk <kamai122000@mail.ru>
2026-06-03 02:09:47 +0300

    asd

--- src/app/(student)/layout.tsx
+++ src/app/(student)/layout.tsx
@@ -1,5 +1,7 @@
-export const dynamic = 'force-dynamic'
-
-export default function StudentLayout({ children }: { children: React.ReactNode }) {
-	return children
+export default function StudentLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return children;
 }
